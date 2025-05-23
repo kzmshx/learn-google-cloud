66e902bf03607647d00223e258b2f5522e032d6e
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-23 23:59:38 +0900

    feat(terraform/gsp752): add Google Cloud Storage bucket and provider configuration

---
+++ terraform/gsp752/main.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket" "test-bucket-for-state" {
+  name                        = var.project_id
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
---
+++ terraform/gsp752/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "google cloud project id"
+  type        = string
+}
+
+variable "region" {
+  description = "google cloud region"
+  type        = string
+}
