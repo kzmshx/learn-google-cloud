3d7d997ca9a32bf2113a6ffefe2072289252fa5c
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-23 22:09:54 +0900

    feat(terraform/gsp750): add Google Compute Engine instance configuration to main.tf

--- terraform/gsp750/main.tf
+++ terraform/gsp750/main.tf
@@ -1,3 +1,19 @@
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+}
