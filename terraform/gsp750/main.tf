3fa4892e6162df44c66db241308ecd25dc4db195
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-23 22:21:50 +0900

    feat(terraform/gsp750): add static IP address resource for VM instance

--- terraform/gsp750/main.tf
+++ terraform/gsp750/main.tf
@@ -2,6 +2,10 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-micro"
