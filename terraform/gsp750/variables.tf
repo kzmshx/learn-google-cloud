877bb02e70c76d735ffdfcc86baa978d18b680ad
Kazumasa Hirata <k_hirata@openwork.co.jp>
2025-05-23 21:56:27 +0900

    feat(terraform): add initial Terraform configuration for Google Cloud with VPC network and provider settings

---
+++ terraform/gsp750/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "GCPのプロジェクトID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCPのリージョン"
+  type        = string
+}
+
+variable "zone" {
+  description = "GCPのゾーン"
+  type        = string
+}
