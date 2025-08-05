50172659d85df6e9715a2fdf178ae1dbf26aeb2a
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2025-08-05 23:32:34 +0900

    refactor(labs/sh-template): remove unused options and streamline role granting function in run script

--- labs/sh-template/run.sh
+++ labs/sh-template/run.sh
@@ -9,30 +9,26 @@ set -eux
 # --------------------------------
 while [[ $# -gt 0 ]]; do
   case $1 in
-  --region) REGION="$2" && shift 2 ;;
-  --zone) ZONE="$2" && shift 2 ;;
   --* | -*) echo "unknown option: $1" >&2 && exit 1 ;;
   *) break ;;
   esac
 done
 
-echo "region: $REGION"
-echo "zone: $ZONE"
-
 # --------------------------------
 # Constants
 # --------------------------------
 PROJECT_ID=$(gcloud config get-value project)
-PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
-COMPUTE_SERVICE_ACCOUNT=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com
 
 # --------------------------------
 # Functions
 # --------------------------------
+function enable_apis() {
+  local APIS=("${@:1}")
+  for API in "${APIS[@]}"; do gcloud services enable --project $PROJECT_ID $API; done
+}
+
 function grant_service_account_roles() {
   local SERVICE_ACCOUNT=$1
   local ROLES=("${@:2}")
-  for ROLE in "${ROLES[@]}"; do
-    gcloud projects add-iam-policy-binding $PROJECT_ID --member "serviceAccount:$SERVICE_ACCOUNT" --role "$ROLE"
-  done
+  for ROLE in "${ROLES[@]}"; do gcloud projects add-iam-policy-binding $PROJECT_ID --member "serviceAccount:$SERVICE_ACCOUNT" --role "$ROLE"; done
 }
