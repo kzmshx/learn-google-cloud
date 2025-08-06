f7b77958984defc8b481b43afb983361c3345f2e
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2025-08-06 09:43:12 +0900

    refactor(labs/sh-template): clean up run script by removing unused argument parsing and commenting out functions

--- labs/sh-template/run.sh
+++ labs/sh-template/run.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
 PS4='\033[1;94m[ Line ${LINENO} ] \033[0m'
-
 set -eux
-
-# --------------------------------
-# Parse args
-# --------------------------------
-while [[ $# -gt 0 ]]; do
-  case $1 in
-  --* | -*) echo "unknown option: $1" >&2 && exit 1 ;;
-  *) break ;;
-  esac
-done
 
 # --------------------------------
 # Constants
@@ -20,15 +9,25 @@ done
 PROJECT_ID=$(gcloud config get-value project)
 
 # --------------------------------
-# Functions
+# Args
 # --------------------------------
-function enable_apis() {
-  local APIS=("${@:1}")
-  for API in "${APIS[@]}"; do gcloud services enable --project $PROJECT_ID $API; done
-}
+# while [[ $# -gt 0 ]]; do
+#   case $1 in
+#   --* | -*) echo "unknown option: $1" >&2 && exit 1 ;;
+#   *) break ;;
+#   esac
+# done
 
-function grant_service_account_roles() {
-  local SERVICE_ACCOUNT=$1
-  local ROLES=("${@:2}")
-  for ROLE in "${ROLES[@]}"; do gcloud projects add-iam-policy-binding $PROJECT_ID --member "serviceAccount:$SERVICE_ACCOUNT" --role "$ROLE"; done
-}
+# --------------------------------
+# Utilities
+# --------------------------------
+# function enable_apis() {
+#   local APIS=("${@:1}")
+#   for API in "${APIS[@]}"; do gcloud services enable --project $PROJECT_ID $API; done
+# }
+
+# function grant_service_account_roles() {
+#   local SERVICE_ACCOUNT=$1
+#   local ROLES=("${@:2}")
+#   for ROLE in "${ROLES[@]}"; do gcloud projects add-iam-policy-binding $PROJECT_ID --member "serviceAccount:$SERVICE_ACCOUNT" --role "$ROLE"; done
+# }
